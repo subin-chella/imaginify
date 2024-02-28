f55c3c9b6efbc69783cf3cc8368b38de5ebaf98b
subin-chella <subin8899@gmail.com>
2024-02-28 18:17:52 +0530

    Added left nav

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,18 +1,53 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { SignedIn } from "@clerk/nextjs";
+import { navLinks } from "../../constants/index";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <aside className="sidebar">
-        <div className='flex size-full flex-col gap-4'>
-            <Link href="/" className="sidebar-logo">
-            <Image src="/assets/images/logo-text.svg" alt="logo" width = {180} height={28}/>
-                
-            </Link>
-        </div>
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image
+            src="/assets/images/logo-text.svg"
+            alt="logo"
+            width={180}
+            height={28}
+          />
+        </Link>
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link className='sidebar-link' href={link.route} >
+                        <Image src={link.icon} alt='logo'width={24} height={24}
+                        className={`${isActive && 'brightness-200'}`}/>
+                        {link.label}
+                    </Link>
+                    
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
+      </div>
     </aside>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
